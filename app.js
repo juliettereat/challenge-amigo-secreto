f7aa1f352cc7b94b2b0142468926a5481d81bf27
juliettereat <reatiga3005@gmail.com>
2025-01-28 12:42:03 -0600

    añade amigos y los muestra en la consola

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Estableciendo el array
 let amigos = [];
-
+let amigo;
 
 
 function agregarAmigo(nuevoAmigo) {
-let amigoSecreto = document.getElementById(nuevoAmigo);
+ amigo = document.getElementById(amigo);
 //agregar un nuevo amigo al array
 if (nuevoAmigo !== "") {
     amigos.push(nuevoAmigo);
-    console.log(amigo agregado `${nuevoAmigo}`);
+    console.log(`amigo agregado "${nuevoAmigo}"`);
 } else { //alert en caso no valido
     alert("Por favor, ingresa un nombre valido")
 }
