3f45dcd295010980b53f1ad49f25a51a95f89f53
juliettereat <reatiga3005@gmail.com>
2025-01-27 19:25:25 -0600

    estableciendo el array, agregar un nuevo amigo

---
+++ app.js
@@ -0,0 +1,10 @@
+//Estableciendo variables
+let amigos = [];
+
+function agregarAmigo(nuevoAmigo) {
+let amigoSecreto = document.querySelector(nuevoAmigo);
+//agregar un nuevo amigo al array
+amigos.push(nuevoAmigo);
+console.log(amigoSecreto);
+}
+
