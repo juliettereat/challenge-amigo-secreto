68f0369a12a99456ec5d8ce80bdc87d741a7219e
juliettereat <reatiga3005@gmail.com>
2025-01-29 23:07:00 -0600

    mostrando la lista en la pantalla

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ function agregarAmigo(nuevoAmigo) {
 //agregar un nuevo amigo al array
 if (nuevoAmigo !== "") {
     amigos.push(nuevoAmigo);
-    console.log(`amigo agregado "${nuevoAmigo}"`);
+const nuevoElementoLista = document.createElement("li"); //crea la lista
+nuevoElementoLista.textContent = nuevoAmigo;// agrega el nombre del amigo al listado
+listaAmigos.appendChild(nuevoElementoLista);//agrega el nombre del amigo a la lista que va a mostrar
 } else { //alert en caso no valido
     alert("Por favor, ingresa un nombre valido")
 }
